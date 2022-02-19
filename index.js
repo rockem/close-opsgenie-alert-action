0a2a3be7a4d7d9f4e2b2199650e3da673eb4a46e
Eli Segal <eli.segal@gmail.com>
2022-02-19 19:58:16 +0200

    wip

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const core = require('@actions/core');
 const opsgenie = require('opsgenie-sdk');
 const { connectionOptions } = require('./src/connection');
 
-opsgenie.configure(connectionOptions(core.getInput('api_key'), core.getInput('using_eu')));
+opsgenie.configure(
+  connectionOptions(core.getInput('api_key'), core.getInput('using_eu_url')));
 
-const alert_identifier = {
-    identifier: core.getInput('alias'),
-    identifierType: "alias"
+const alertIdentifier = {
+  identifier: core.getInput('alias'),
+  identifierType: "alias"
 };
 
-const close_alert_data = {
-    note: core.getInput('note'),
-    user: core.getInput('user')
+const closeAlertData = {
+  note: core.getInput('note'),
+  user: core.getInput('user')
 };
 
-opsgenie.alertV2.close(alert_identifier, close_alert_data, function (error) {
-    if (error) {
-        core.setFailed(`ERROR: ${error.message}`);
-    } else {
-        console.log(`Request sent for closing alert with alias: ${core.getInput('alias')}`);
-    }
+opsgenie.alertV2.close(alertIdentifier, closeAlertData, function (error) {
+  if (error) {
+    core.setFailed(`ERROR: ${error.message}`);
+  } else {
+    console.log(`Request sent for closing alert with alias: ${core.getInput('alias')}`);
+  }
 });
 
